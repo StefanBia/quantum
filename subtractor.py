221fb4c9c6b43691b686956c483674dc1b9ce621
Stefan Bia <stefanbia0108@gmail.com>
2024-10-21 18:50:12 +0300

    Half subtractor

---
+++ subtractor.py
@@ -0,0 +1,43 @@
+from qiskit import QuantumCircuit, transpile
+from qiskit.visualization import plot_histogram
+from qiskit_aer import AerSimulator
+import matplotlib.pyplot as plt
+
+# Create a Quantum Circuit with 2 qubits and 2 classical bits
+circuit = QuantumCircuit(4)
+
+initial_state = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+circuit.initialize(initial_state, [0, 1, 2, 3])
+
+# Apply gates to the circuit
+circuit.x(0)
+circuit.ccx(0, 1, 3)
+circuit.ccx(0, 1, 2)
+circuit.x(0)
+circuit.x(1)
+circuit.ccx(0, 1, 2)
+circuit.x(1)
+
+circuit.draw('mpl')
+plt.show()
+
+simulator = AerSimulator()
+
+# Transpile the circuit for the simulator
+
+circuit.measure_all()
+
+# Re-transpile the circuit with measurements
+compiled_circuit = transpile(circuit, simulator)
+
+# Run the simulation, specifying the number of shots (repetitions)
+job = simulator.run(compiled_circuit, shots=1024)
+result = job.result()
+
+# Get the counts of the measurement results
+counts = result.get_counts(compiled_circuit)
+print(counts)
+
+# Plot a histogram of the results
+plot_histogram(counts)
+plt.show()
