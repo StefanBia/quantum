a6e8c036d97d49a23c1a9f09f1bb4352816a4a77
Stefan Bia <stefanbia0108@gmail.com>
2024-10-21 17:29:55 +0300

    Quantum Full adder

---
+++ bell_state.py
@@ -0,0 +1,40 @@
+from qiskit import QuantumCircuit, transpile
+from qiskit.visualization import plot_histogram
+from qiskit_aer import AerSimulator
+import matplotlib.pyplot as plt
+
+# Create a Quantum Circuit with 2 qubits and 2 classical bits
+circuit = QuantumCircuit(4)
+
+initial_state = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+circuit.initialize(initial_state, [0, 1, 2, 3])
+
+# Apply gates to the circuit
+circuit.ccx(0, 1, 3)
+circuit.cx(0, 1)
+circuit.ccx(1, 2, 3)
+circuit.cx(1, 2)
+circuit.cx(0, 1)
+
+# Visualize the circuit
+circuit.draw('mpl')
+plt.show()
+
+simulator = AerSimulator()
+
+# Re-transpile the circuit with measurements
+compiled_circuit = transpile(circuit, simulator)
+
+# Run the simulation, specifying the number of shots (repetitions)
+job = simulator.run(compiled_circuit, shots=1024)
+result = job.result()
+
+# Get the counts of the measurement results
+counts = result.get_counts(compiled_circuit)
+print(counts)
+
+# Plot a histogram of the results
+plot_histogram(counts)
+plt.show()
+
+
